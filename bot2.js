15685cf22d946c62b56a3a15a834fbcad27039c1
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2018-11-18 02:06:53 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -256,7 +256,7 @@ function play(guild, song) {
 }
 });
 client.on('message' , message => {
-  var prefix = "x";
+  var prefix = "-";
   if(message.author.bot) return;
  
   if(message.content.startsWith(prefix + "xo")) {
